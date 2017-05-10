9b125f83e05c1d5225e0c48b54d05853fa675d5b
stone <yue.19901010@163.com>
2017-05-10 15:43:05 +0800

    创建

---
+++ src/main/java/com/stone/CatchdemoApplication.java
@@ -0,0 +1,20 @@
+package com.stone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SpringBootApplication
+public class CatchdemoApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "index";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchdemoApplication.class, args);
+	}
+}
